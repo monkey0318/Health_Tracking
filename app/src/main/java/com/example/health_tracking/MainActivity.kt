873384868bffba6b15e90dbbf87a4421600fec84
Yong Sen <yongsen318@gmail.com>
2023-12-28 21:32:23 +0800

    main activity

--- app/src/main/java/com/example/health_tracking/MainActivity.kt
+++ app/src/main/java/com/example/health_tracking/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.health_tracking
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnLogin :Button
+    private lateinit var btnRegister: Button
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnLogin = findViewById(R.id.btnLogin)
+        btnRegister = findViewById(R.id.btnRegister)
+
+        btnLogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+        btnRegister.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
