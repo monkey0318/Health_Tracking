37651696b6c55210c23e67071c6b5900108dd4df
HISOKA_AN <qfengqin@gmail.com>
2024-01-01 16:42:33 +0800

    Register and Login Suscess

--- app/src/main/java/com/example/health_tracking/RegisterActivity.kt
+++ app/src/main/java/com/example/health_tracking/RegisterActivity.kt
@@ -1,15 +1,25 @@
 package com.example.health_tracking
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.auth.User
+import com.google.firebase.firestore.firestore
 
 class RegisterActivity: AppCompatActivity() {
 
@@ -17,12 +27,15 @@ class RegisterActivity: AppCompatActivity() {
     lateinit var editEmailText: EditText
     lateinit var editPasswordText: EditText
 
-    data class User(val name:String, val email: String, val password: String)
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var databaseReference: DatabaseReference
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
-
+        val db = Firebase.firestore
 
         editNameText= findViewById(R.id.editNameText)
         editEmailText= findViewById(R.id.editEmailText)
@@ -36,6 +49,9 @@ class RegisterActivity: AppCompatActivity() {
         registerBtn.setOnClickListener { checkEmpty(it) }
         loginTextView.setOnClickListener { loginAccount(it) }
 
+        mAuth = FirebaseAuth.getInstance()
+        databaseReference = FirebaseDatabase.getInstance().reference
+
     }
 
     private fun checkEmpty(view: View){
@@ -44,21 +60,33 @@ class RegisterActivity: AppCompatActivity() {
         val password = editPasswordText.text.toString()
 
         if (name.isNotEmpty() && email.isNotEmpty() && password.isNotEmpty()) {
-            //store data
+            // Store data
+            val user = hashMapOf(
+                "name" to name,
+                "email" to email,
+                "password" to password
+            )
 
-            val user= User(name,email,password)
-              val database = FirebaseDatabase.getInstance()
-             val usersRef = database.getReference("users")
-             val userId = usersRef.push().key
-             usersRef.child(userId!!).setValue(user)
 
-            loginAccount(view)
+            val db = FirebaseFirestore.getInstance()
 
-        }else {
+            // Add a new document to the "users" collection with auto-generated ID
+            db.collection("users")
+                .add(user)
+                .addOnSuccessListener { documentReference ->
 
+                    Toast.makeText(applicationContext, "Register Successfully", Toast.LENGTH_SHORT).show()
+                    loginAccount(view)
+                }
+                .addOnFailureListener { e ->
+                    Toast.makeText(applicationContext, "Something went wrong!!", Toast.LENGTH_SHORT).show()
+
+
+                }
+
+        } else {
             Toast.makeText(this, "Do not leave empty", Toast.LENGTH_SHORT).show()
         }
-
     }
 
     private fun loginAccount(view: View) {
